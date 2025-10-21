matthewdowney/cometbft

[STAB-17] Make unsynchronized local client (#3)
This is effectively a copy of the fully unsynchronized local client found in https://github.com/tendermint/tendermint/pull/9660. Note that this was reverted and replaced by a version that has a mutex per instance of the client in https://github.com/tendermint/tendermint/pull/9830. This change can be removed once a fully unsynchronized client is added back.

proxy/unsync_local_client_creator.go
@@ -1,0 +1,24 @@
+package proxy
+
+import (
+	abcicli "github.com/tendermint/tendermint/abci/client"
+	"github.com/tendermint/tendermint/abci/types"
+)
+
+var (
+	_ ClientCreator = (*unsynchronizedClientCreator)(nil)
+)
+
+// NewUnsynchronizedLocalClientCreator creates a local client that is unsynchronized. It is expected that the
+// provided application perform all synchronization necessary to prevent unexpected results.
+func NewUnsynchronizedLocalClientCreator(app types.Application) ClientCreator {
+	return &unsynchronizedClientCreator{app: app}
+}
+
+type unsynchronizedClientCreator struct {
+	app types.Application
+}
+
+func (u unsynchronizedClientCreator) NewABCIClient() (abcicli.Client, error) {
+	return abcicli.NewUnsyncLocalClient(u.app), nil
+}

01builders/dydx-cometbft

proxy/unsync_local_client_creator.go
@@ -1,0 +1,24 @@
+package proxy
+
+import (
+	abcicli "github.com/cometbft/cometbft/abci/client"
+	"github.com/cometbft/cometbft/abci/types"
+)
+
+var (
+	_ ClientCreator = (*unsynchronizedClientCreator)(nil)
+)
+
+// NewUnsynchronizedLocalClientCreator creates a local client that is unsynchronized. It is expected that the
+// provided application perform all synchronization necessary to prevent unexpected results.
+func NewUnsynchronizedLocalClientCreator(app types.Application) ClientCreator {
+	return &unsynchronizedClientCreator{app: app}
+}
+
+type unsynchronizedClientCreator struct {
+	app types.Application
+}
+
+func (u unsynchronizedClientCreator) NewABCIClient() (abcicli.Client, error) {
+	return abcicli.NewUnsyncLocalClient(u.app), nil
+}